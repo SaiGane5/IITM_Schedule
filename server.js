32ffa462dce894c2548cfaa03044c88dc1fd6f37
SaiGane5 <imandisaiganesh1@gmail.com>
2024-08-06 15:08:23 +0530

    Update server & To-Do styling

--- server.js
+++ server.js
@@ -9,25 +9,22 @@ import cors from 'cors';
 import methodOverride from 'method-override';
 import dotenv from "dotenv";
 
-
-
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = 3000;
 const app = express();
-// Add methodOverride middleware
 app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+
+dotenv.config();
 
 const pool = new Pool({
     user: process.env.DB_USER,
@@ -37,10 +34,9 @@ const pool = new Pool({
     port: process.env.DB_PORT,
 });
 
-// To-Do Routes
 app.get('/todos', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM todos');
+        const result = await pool.query('SELECT * FROM todos ORDER BY id ASC');
         res.render('todo', { todos: result.rows });
     } catch (error) {
         res.status(500).send(error);
@@ -51,7 +47,7 @@ app.post('/todos', async (req, res) => {
     const { task } = req.body;
     try {
         await pool.query('INSERT INTO todos (task) VALUES ($1)', [task]);
-        res.redirect('/todos'); // Redirect to the todos page
+        res.redirect('/todos');
     } catch (error) {
         res.status(500).send(error);
     }
@@ -61,8 +57,12 @@ app.put('/todos/:id', async (req, res) => {
     const { id } = req.params;
     const { completed } = req.body;
     try {
-        await pool.query('UPDATE todos SET completed = $1 WHERE id = $2', [completed, id]);
-        res.sendStatus(204);
+        if (completed === 'true') {
+            await pool.query('UPDATE todos SET completed = $1, completed_at = $2 WHERE id = $3', [completed, new Date(), id]);
+        } else {
+            await pool.query('UPDATE todos SET completed = $1, completed_at = NULL WHERE id = $2', [completed, id]);
+        }
+        res.redirect('/todos');
     } catch (error) {
         res.status(500).send(error);
     }
@@ -72,7 +72,7 @@ app.delete('/todos/:id', async (req, res) => {
     const { id } = req.params;
     try {
         await pool.query('DELETE FROM todos WHERE id = $1', [id]);
-        res.sendStatus(204);
+        res.redirect('/todos');
     } catch (error) {
         res.status(500).send(error);
     }
@@ -135,7 +135,7 @@ app.get('/', (req, res) => {
                 return;
             }
         }
-        res.send('<body style="display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; background-color: #f0f0f0; font-family: Arial;"><div style="background-color: #fff; padding: 20px; border-radius: 5px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);"><h1 style="color: #333; text-align: center;">No current classes.</h1></div></body>');
+        res.send('<body style="display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; background-color: #f0f0f0; font-family: Arial;"><div style="background-color: #fff; padding: 20px; border-radius: 5px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);"><h1 style="color: #333; text-align: center;">No current classes.<div class="navigation"><a style="text-decoration: none; color: #333;" href = "/todos" >To-Do</a ></div></h1></div></body>');
     }
 });
 
